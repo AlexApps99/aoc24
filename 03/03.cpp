fd66ed2c267ae6a5a141a7aed01942030ad54b2f
AlexApps99 <alex.apps99@gmail.com>
2024-12-03 18:37:40 +1100

    Clean up 03 a bit

--- 03/03.cpp
+++ 03/03.cpp
@@ -1,54 +1,43 @@
 #include <cstddef>
 #include <cstdint>
+#include <cstdlib>
 #include <iostream>
 #include <iterator>
-#include <sstream>
 #include <string>
-#include <vector>
-
-bool check_next_keyword(const std::string input, size_t &i,
-                        const std::string keyword) {
-    for (size_t j = 0; j < keyword.size(); j++) {
-        if (i > input.length()) {
-            return false;
-        }
-        if (input[i] != keyword[j]) {
-            return false;
-        }
-        i += 1;
-    }
-    return true;
-}
 
 // this must be called when i points to "m",
 // it seeks forward to the first char that doesn't match
-// the pattern
-bool check_next_mul(const std::string input, size_t &i, uint64_t &result) {
-    uint64_t a = 0, b = 0;
+// the pattern mul\([0-9]+,[0-9]+\).
+// returns number to be added.
+uint64_t scan_mul(const std::string &input, size_t &i) {
     size_t next_bad_val = 0;
 
+    const std::string preamble{"mul("};
+
     // seek forward as appropriate here
-    if (!check_next_keyword(input, i, "mul(")) {
-        return false;
+    for (char k : preamble) {
+        if (i > input.length() || input[i] != k) {
+            return 0;
+        }
+        i += 1;
     }
 
     if (i > input.length()) {
-        goto buffer_end;
+        return 0;
     }
 
     // find next non-number
     next_bad_val = input.find_first_not_of("0123456789", i);
     if (next_bad_val == std::string::npos) {
-        goto buffer_end;
+        i = input.length();
+        return 0;
     }
     if (next_bad_val == i) {
         // keep searching from this point
-        return false;
+        return 0;
     }
-    {
-        std::string a_str = input.substr(i, next_bad_val - i);
-        a = std::strtoul(a_str.c_str(), nullptr, 10);
-    }
+    const std::string a_str = input.substr(i, next_bad_val - i);
+    const uint64_t a = std::strtoul(a_str.c_str(), nullptr, 10);
     i = next_bad_val;
 
     // at this point, we should have something like this:
@@ -57,47 +46,40 @@ bool check_next_mul(const std::string input, size_t &i, uint64_t &result) {
 
     if (input[i] != ',') {
         // keep searching from this point
-        return false;
+        return 0;
     }
     i += 1;
     if (i > input.length()) {
-        goto buffer_end;
+        return 0;
     }
 
     // find next non-number
     next_bad_val = input.find_first_not_of("0123456789", i);
     if (next_bad_val == std::string::npos) {
-        goto buffer_end;
+        i = input.length();
+        return 0;
     }
     if (next_bad_val == i) {
         // keep searching from this point
-        return false;
+        return 0;
     }
-    {
-        std::string b_str = input.substr(i, next_bad_val - i);
-        b = std::strtoul(b_str.c_str(), nullptr, 10);
-    }
+    const std::string b_str = input.substr(i, next_bad_val - i);
+    const uint64_t b = std::strtoul(b_str.c_str(), nullptr, 10);
     i = next_bad_val;
 
     if (input[i] != ')') {
         // keep searching from this point
-        return false;
+        return 0;
     }
     i += 1;
 
-    result += a * b;
-
-    return true;
-
-buffer_end:
-    i = input.length();
-    return false;
+    return a * b;
 }
 
 // NOLINTNEXTLINE(readability-function-cognitive-complexity)
 void aoc_03(std::istream &in, std::string &out1, std::string &out2) {
     // read input to string
-    std::string input{std::istreambuf_iterator<char>(in), {}};
+    const std::string input{std::istreambuf_iterator<char>(in), {}};
 
     uint64_t result_off = 0;
     uint64_t result_on = 0;
@@ -105,28 +87,30 @@ void aoc_03(std::istream &in, std::string &out1, std::string &out2) {
 
     size_t i = 0;
 
+    const std::string DO_FUNC{"do()"};
+    const std::string DONT_FUNC{"don't()"};
+
     while (i < input.length()) {
         if (input[i] == 'm') {
-            check_next_mul(input, i, is_mul_on ? result_on : result_off);
-            continue;
+            const uint64_t scan_result = scan_mul(input, i);
+            if (is_mul_on) {
+                result_on += scan_result;
+            } else {
+                result_off += scan_result;
+            }
         } else if (input[i] == 'd') {
-            if (input.substr(i, 4) == "do()") {
+            if (input.substr(i, DO_FUNC.length()) == DO_FUNC) {
                 is_mul_on = true;
-                i += 4;
-                continue;
-            } else if (input.substr(i, 7) == "don't()") {
+                i += DO_FUNC.length();
+            } else if (input.substr(i, DONT_FUNC.length()) == DONT_FUNC) {
                 is_mul_on = false;
-                i += 7;
-                continue;
+                i += DONT_FUNC.length();
             } else {
                 i += 1;
-                continue;
             }
         } else {
+            // will be > input.length() if none found
             i = input.find_first_of("dm", i);
-            if (i == std::string::npos) {
-                i = input.length();
-            }
         }
     }
 
